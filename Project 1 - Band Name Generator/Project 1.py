8eff3f29dc226b1a96661f9b9068aeaf45694b8f
nguyenvuongminh <147414299+nguyenvuongminh@users.noreply.github.com>
2025-07-16 11:41:18 +0700

    add the Project 1 code from the laptop

---
+++ Project 1 - Band Name Generator/Project 1.py
@@ -0,0 +1,5 @@
+print('Hello there!\n')
+city = input('What is the name of the city you grew up in?\n')
+pet = input('What is the name of your first pet?\n')
+print("Your band name should be " + city + " " + pet + '.')
+
