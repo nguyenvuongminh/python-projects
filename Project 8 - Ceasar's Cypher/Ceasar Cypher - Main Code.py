16566a6aca7027a6f783bcf99f7ebe263e5a8ea5
Minh Nguyen <147414299+nguyenvuongminh@users.noreply.github.com>
2025-08-02 18:48:44 +0700

    Update Ceasar Cypher - Main Code.py

--- Project 8 - Ceasar's Cypher/Ceasar Cypher - Main Code.py
+++ Project 8 - Ceasar's Cypher/Ceasar Cypher - Main Code.py
@@ -6,28 +6,51 @@
 # Written by Minh Nguyen, August 1st, 2025.
 ###****************************************************************
 
-### SETUP
+### ALPHABET
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-selection = input('What do you want to do, encrypt or decrypt?\n')
-final_code = []
+
+switch = 'ON'
 
 ### ENCRYPTING FUNCTION
 def encrypt(alphabet, original_code, shifting_number, final_code):
     for letter in original_code:
-        index = original_code.index(letter)
-        new_letter = alphabet[index + shifting_number]
+        index = alphabet.index(letter)
+        if (index + shifting_number) > 26:
+            new_letter = alphabet[index + shifting_number - 26]
+        else:
+            new_letter = alphabet[index + shifting_number - 26]
         final_code.append(new_letter)
 
 ### DECRYPTING FUNCTION
-def encrypt(alphabet, original_code, shifting_number, final_code):
+def decrypt(alphabet, original_code, shifting_number, final_code):
     for letter in original_code:
-        index = original_code.index(letter)
-        new_letter = alphabet[index + shifting_number]
+        index = alphabet.index(letter)
+        new_letter = alphabet[index - shifting_number]
         final_code.append(new_letter)
 
-### ENCRYPTING CASE
-code = input('Please enter your code:\n')
-original_code = list(code)
-shifting_number = int(input('Please enter your desired shifting number:\n'))
+### THE LOOP
+while switch == 'ON':
+    selection = input('What do you want to do, encrypt or decrypt?\n')
+    code = input('Please enter your code:\n')
+    original_code = list(code)
+    final_code = []
+    shifting_number = int(input('Please enter your desired shifting number:\n'))
+
+    if selection == 'encrypt':
+        encrypt(alphabet, original_code, shifting_number, final_code)
+        print(''.join(final_code))
+    elif selection == 'decrypt':
+        decrypt(alphabet, original_code, shifting_number, final_code)
+        print(''.join(final_code))
+
+    resume = input('Do you want to continue? Please type "yes" or "no".\n')
+
+    if resume == 'yes':
+        switch = 'ON'
+    else:
+        print('Thanks for using the tool!')
+        switch = 'OFF'
+
+
 
 
