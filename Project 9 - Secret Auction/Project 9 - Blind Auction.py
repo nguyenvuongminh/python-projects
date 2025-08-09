843ffe7c3fe4942f75c7ef8ae18d2a1cfdb6681f
Minh Nguyen <147414299+nguyenvuongminh@users.noreply.github.com>
2025-08-09 23:14:48 +0700

    Update Project 9 - Blind Auction.py

--- Project 9 - Secret Auction/Project 9 - Blind Auction.py
+++ Project 9 - Secret Auction/Project 9 - Blind Auction.py
@@ -15,16 +15,26 @@ print(logo + '\n')
 other = 'yes'
 
 bids = {}
+highest_bid = 0
 
 while other == 'yes':
     name = input('What is your name? ')
     price = int(input('What is your bid? $'))
-    other = input('Is there any other bidder? Type "yes" or "no".')
+    other = input('Is there any other bidder? Type "yes" or "no". ')
 
     bids[name] = price
 
     if other == 'yes':
-        print('\n * 20')
+        print('\n'* 20)
     elif other == 'no':
         break
+
+for name in bids:
+    if bids[name] > highest_bid:
+        highest_bid = bids[name]
+        highest_bidder = name
+
+print(f'Congratulation {highest_bidder}! You have won the secret auction!')
+
+
 
